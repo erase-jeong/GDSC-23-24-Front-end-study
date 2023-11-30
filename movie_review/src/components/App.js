782747cd6f18135ce44d4e371ac8a8c5772c77fb
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2023-11-30 22:48:41 +0900

    페이지를 열었을 때 알아서 데이터를 불러오도록 변경

--- movie_review/src/components/App.js
+++ movie_review/src/components/App.js
@@ -17,10 +17,13 @@ function App(){
         setItems(nextItems);
     }
 
-    const handleLoadClick=async()=>{
+    const handleLoad=async()=>{
         const {reviews}=await getReviews();
         setItems(reviews);
     }
+
+    handleLoad();
+    
 
     return (
         <div>
@@ -29,7 +32,6 @@ function App(){
                 <button onClick={handleBestClick}>베스트순</button>
             </div>
          <ReviewList items={sortedItems} onDelete={handleDelete}/>
-         <button onClick={handleLoadClick}>불러오기</button>
         </div>
     )
 }
