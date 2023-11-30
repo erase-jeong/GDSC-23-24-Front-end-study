51bc6a03c42ecef474a2075f227fcd2134b328d6
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2023-11-30 21:15:50 +0900

    컴포넌트 기본 세팅

---
+++ movie_review/src/components/ReviewList.js
@@ -0,0 +1,8 @@
+
+
+function ReviewList({items}){
+    console.log(items);
+    return <ul></ul>;
+}
+
+export default ReviewList;
--- movie_review/src/index.js
+++ movie_review/src/index.js
@@ -1,9 +1,4 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <h1>[index.js] React 프로젝트 입니다.</h1>
-
-);
+ReactDOM.render(<App />, document.getElementById('root'));
