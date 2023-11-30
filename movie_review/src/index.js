2d5ba548cf8e60b9e664606f246e7121a7fe1f2d
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2023-11-30 20:17:28 +0900

    react 초기 세팅

---
+++ movie_review/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <h1>[index.js] React 프로젝트 입니다.</h1>
+
+);
