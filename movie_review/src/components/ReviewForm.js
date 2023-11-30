a0e54d79ef9b87c797a6a9d98c1c41476ddb8a30
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2023-12-01 08:45:06 +0900

    form으로 제목, 평점, 내용 입력받기

--- movie_review/src/components/ReviewForm.js
+++ movie_review/src/components/ReviewForm.js
@@ -1,17 +1,29 @@
 import {useState} from 'react';
+import './ReviewForm.css';
 
 function ReviewForm(){
     const [title, setTitle]=useState('');
+    const [rating, setRating]=useState(0);
+    const [content, setContent]=useState('');
 
     const handleTitleChange=(e)=>{
-        //input의 value가 변경될 때마다 그 값을 state에 반영
-        //setTitle함수를 써서 타겟값 변경
         setTitle(e.target.value);
     }
 
+    const handleRatingChange=(e)=>{
+        const nextRating=Number(e.target.value)||0;  //or 연산자 사용 -> 입력값이 숫자가 아닌 경우 처리
+        setRating(nextRating);
+    }
+
+    const handleContentChange=(e)=>{
+        setContent(e.target.value);
+    }
+
     return (
-    <form>
-        <input value={title} onChange={handleTitleChange}></input>
+    <form className="ReveiwForm">
+        제목 : <input value={title} onChange={handleTitleChange}></input>
+        평점 : <input type="number" value={rating} onChange={handleRatingChange}></input>
+        내용 : <textarea value={content} onChange={handleContentChange}/>
     </form>
     
     );
@@ -19,8 +31,3 @@ function ReviewForm(){
 
 export default ReviewForm;
 
-/*html에서는 사용자가 input에 입력할 때마다 onInput이라는 데이터가발생했는데, 
-onChange 이벤트는 사용자 입력이 끝났을 때 발생하는 이벤트 
-
-리액트에서 onChange는 순수 자바스크립트에서의 onChange이벤트와 다르게 동작함.  
-*/
