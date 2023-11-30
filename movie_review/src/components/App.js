39822257f81adb766e1c6c48773f3c4340911e6f
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2023-11-30 22:07:31 +0900

    4-2-2) rating(평점) 순으로 정렬

--- movie_review/src/components/App.js
+++ movie_review/src/components/App.js
@@ -3,7 +3,7 @@ import items from '../mock.json';
 import {useState} from 'react';
 
 function App(){
-    const [order,setOrder]=useState('createAt');
+    const [order,setOrder]=useState('rating');
     const sortedItems=items.sort((a,b)=>b[order]-a[order]);
     return (
         <div>
