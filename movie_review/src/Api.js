15b79d257adf2629b7b5da2ab2aa0e5d08a9f1fc
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2023-12-01 05:03:16 +0900

    useEffect 로 초기 데이터 가져오기!

--- movie_review/src/Api.js
+++ movie_review/src/Api.js
@@ -1,5 +1,19 @@
+
 export async function getReviews(){
-    const response=await fetch('https://learn.codeit.kr/8745/film-reviews/');
+    const response=await fetch('https://learn.codeit.kr/0000/film-reviews/');
     const body=await response.json();
     return body;
 }
+
+/*
+export async function getReviews() {
+    try {
+        const response = await fetch('https://learn.codeit.kr/2375/film-reviews/');
+        const body = await response.json();
+        console.log(body);
+        return body;
+    } catch (error) {
+        console.error('리뷰를 가져오는 중 오류 발생:', error);
+    }
+}
+*/
--- movie_review/src/components/App.js
+++ movie_review/src/components/App.js
@@ -1,9 +1,10 @@
-import ReviewList from "./ReviewList";
+import ReviewList from "./ReviewList"
 import {useState, useEffect} from 'react';
-import {getReviews} from '../Api';
+import {getReviews} from '../api';
 
 function App(){
-    const [order,setOrder]=useState('rating');
+    
+    const [order, setOrder]=useState('rating');
     const [items,setItems]=useState([]);
 
     const sortedItems=items.sort((a,b)=>b[order]-a[order]);
@@ -18,12 +19,14 @@ function App(){
     }
 
     const handleLoad=async()=>{
-        const {reviews}=await getReviews();
+        const {reviews} = await getReviews();
         setItems(reviews);
     }
 
-    useEffect(()=>{handleLoad();},[]);
-    
+    useEffect(()=>{
+        handleLoad();
+    }, []);
+
 
     return (
         <div>
@@ -31,9 +34,11 @@ function App(){
                 <button onClick={handleNewestClick}>최신순</button>
                 <button onClick={handleBestClick}>베스트순</button>
             </div>
-         <ReviewList items={sortedItems} onDelete={handleDelete}/>
+            <ReviewList items={sortedItems} onDelete={handleDelete}/>
+            
         </div>
-    )
+
+    );
 }
 
 export default App;
