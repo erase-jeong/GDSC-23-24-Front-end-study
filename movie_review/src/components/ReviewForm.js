5c63cc7ba40adebef4b29b62156dfa07148e4d32
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2023-12-03 00:01:31 +0900

    파일인풋

--- movie_review/src/components/ReviewForm.js
+++ movie_review/src/components/ReviewForm.js
@@ -1,11 +1,20 @@
 import {useState} from 'react';
 import './ReviewForm.css';
+import FileInput from "./FileInput"
 
 function ReviewForm(){
-    const [title, setTitle]=useState('');
+
+    const [values, setValues]=useState({
+        title:'',
+        rating:0,
+        content:'',
+    }); 
+    /*const [title, setTitle]=useState('');
     const [rating, setRating]=useState(0);
     const [content, setContent]=useState('');
+*/
 
+/*
     const handleTitleChange=(e)=>{
         setTitle(e.target.value);
     }
@@ -18,22 +27,34 @@ function ReviewForm(){
     const handleContentChange=(e)=>{
         setContent(e.target.value);
     }
+*/
+    const handleChange=(e)=>{
+        const {name, value}=e.target;
+        setValues((prevValues)=>({
+            ...prevValues,
+            [name]:value,
+            //[name]: name의 값으로 property값을 지정하고
+            //여기에 해당하는 값을 value로 지정
+        }));
+    }
 
     const handleSubmit=(e)=>{
         e.preventDefault();
-        console.log({
-            title,
-            rating,
-            content
-        })
+        console.log(values);
+        // console.log({
+        //     title,
+        //     rating,
+        //     content
+        // })
         //setContent(e.target.value);
     }
 
     return (
     <form className="ReveiwForm" onSubmit={handleSubmit}> <p></p>
-        제목 : <input value={title} onChange={handleTitleChange}></input> <p></p>
-        평점 : <input type="number" value={rating} onChange={handleRatingChange}></input> <p></p> 
-        내용 : <textarea value={content} onChange={handleContentChange}/> <p></p>
+        <FileInput /><p></p>
+        제목 : <input name="title" value={values.title} onChange={handleChange}></input> <p></p>
+        평점 : <input name="rating" type="number" value={values.rating} onChange={handleChange}></input> <p></p> 
+        내용 : <textarea name="content" value={values.content} onChange={handleChange}/> <p></p>
         <button type="submit ">확인</button>
     </form>
     
